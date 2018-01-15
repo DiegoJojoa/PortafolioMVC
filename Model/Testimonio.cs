6679355f94dee7505198d95a3ffccc9306ab0ee1
DiegoJojoa <diegojojoa-94@hotmail.com>
2018-01-15 16:17:13 -0500

    El BackOffice del portafolio terminado.

--- Model/Testimonio.cs
+++ Model/Testimonio.cs
@@ -4,7 +4,9 @@ namespace Model
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity;
     using System.Data.Entity.Spatial;
+    using System.Linq;
 
     [Table("Testimonio")]
     public partial class Testimonio
@@ -28,5 +30,128 @@ namespace Model
         public string fecha { get; set; }
 
         public virtual Usuario Usuario { get; set; }
+
+        public Testimonio Obtener(int id)
+        {
+            var testimonio = new Testimonio();
+            try
+            {
+                using (var ctx = new ProyectoContext())
+                {
+                    testimonio = ctx.Testimonio.Where(x => x.id == id).SingleOrDefault();
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return testimonio;
+        }
+
+
+        public ResponseModel Guardar()
+        {
+            var rm = new ResponseModel();
+            try
+            {
+                using (var ctx = new ProyectoContext())
+                {
+                    if (this.id > 0) ctx.Entry(this).State = EntityState.Modified;
+                    else ctx.Entry(this).State = EntityState.Added;
+
+                    ctx.SaveChanges();
+                    rm.SetResponse(true);
+                }
+            }
+
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return rm;
+        }
+
+
+        public ResponseModel Eliminar(int id)
+        {
+            var rm = new ResponseModel();
+            try
+            {
+                using (var ctx = new ProyectoContext())
+                {
+                    this.id = id;
+                    ctx.Entry(this).State = EntityState.Deleted;
+
+                    ctx.SaveChanges();
+                    rm.SetResponse(true);
+                }
+            }
+
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return rm;
+        }
+
+        public AnexGRIDResponde Listar(AnexGRID grid, int usuario_id)
+        {
+            try
+            {
+                using (var ctx = new ProyectoContext())
+                {
+
+                    ctx.Configuration.LazyLoadingEnabled = false;
+                    grid.Inicializar();
+
+
+
+                    var query = ctx.Testimonio.Where(x => x.usuario_id == usuario_id);
+
+                    if (grid.columna == "id")
+                    {
+                        query = grid.columna_orden == "DESC" ? query.OrderByDescending(x => x.id) : query.OrderBy(x => x.id);
+                    }
+
+                    if (grid.columna == "nombre")
+                    {
+                        query = grid.columna_orden == "DESC" ? query.OrderByDescending(x => x.nombre) : query.OrderBy(x => x.nombre);
+                    }
+
+                    if (grid.columna == "ip")
+                    {
+                        query = grid.columna_orden == "DESC" ? query.OrderByDescending(x => x.ip) : query.OrderBy(x => x.ip);
+                    }
+
+                    if (grid.columna == "fecha")
+                    {
+                        query = grid.columna_orden == "DESC" ? query.OrderByDescending(x => x.fecha) : query.OrderBy(x => x.fecha);
+                    }
+
+
+                    var testimonios = query.Skip(grid.pagina).Take(grid.limite).ToList();
+
+                    var total = query.Count();
+
+                    grid.SetData(testimonios, total);
+
+                }
+            }
+            catch (Exception E)
+            {
+
+                throw;
+            }
+
+            return grid.responde();
+
+        }
+
     }
 }
--- PortafolioMVC/Areas/Admin/Controllers/TestimoniosController.cs
+++ PortafolioMVC/Areas/Admin/Controllers/TestimoniosController.cs
@@ -1,4 +1,6 @@
-﻿using PortafolioMVC.Areas.Admin.Filters;
+﻿using Helper;
+using Model;
+using PortafolioMVC.Areas.Admin.Filters;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,10 +12,48 @@ namespace PortafolioMVC.Areas.Admin.Controllers
    [Autenticado]
     public class TestimoniosController : Controller
     {
+
+        private Testimonio Testimonio = new Testimonio();
+
         // GET: Admin/Testimonios
         public ActionResult Index()
         {
             return View();
+        }
+
+
+        public JsonResult Listar(AnexGRID grid)
+        {
+            return Json(Testimonio.Listar(grid, SessionHelper.GetUser()));
+        }
+
+        public ActionResult Crud(int id)
+        {
+            return View(Testimonio.Obtener(id));
+        }
+
+
+        public JsonResult Guardar(Testimonio model)
+        {
+            var rm = new ResponseModel();
+
+            if (ModelState.IsValid)
+            {
+                rm = model.Guardar();
+                if (rm.response)
+                {
+                    rm.href = Url.Content("~/Admin/Testimonios/");
+                }
+            }
+
+            return Json(rm);
+        }
+
+
+        public JsonResult Eliminar(int id)
+        {
+
+            return Json(Testimonio.Eliminar(id));
         }
     }
 }
