471c79d872c9a4d8b8aeb38d89adbd7c829a3a48
DiegoJojoa <diegojojoa-94@hotmail.com>
2018-01-15 02:06:56 -0500

    Terminada la sección de habilidades.

--- Model/Habilidad.cs
+++ Model/Habilidad.cs
@@ -4,6 +4,7 @@ namespace Model
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity;
     using System.Data.Entity.Spatial;
     using System.Linq;
 
@@ -27,6 +28,124 @@ namespace Model
             using(var ctx=new ProyectoContext()) {
                 return ctx.Habilidad.Count();
             }
+        }
+
+
+        public Habilidad Obtener(int id)
+        {
+            var habilidad = new Habilidad();
+            try
+            {
+                using (var ctx = new ProyectoContext())
+                {
+                    habilidad = ctx.Habilidad.Where(x => x.id == id).SingleOrDefault();
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return habilidad;
+        }
+
+
+        public ResponseModel Guardar()
+        {
+            var rm = new ResponseModel();
+            try
+            {
+                using (var ctx = new ProyectoContext())
+                {
+                    if (this.id > 0) ctx.Entry(this).State = EntityState.Modified;
+                    else ctx.Entry(this).State = EntityState.Added;
+
+                    ctx.SaveChanges();
+                    rm.SetResponse(true);
+                }
+            }
+
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return rm;
+        }
+
+
+        public ResponseModel Eliminar(int id)
+        {
+            var rm = new ResponseModel();
+            try
+            {
+                using (var ctx = new ProyectoContext())
+                {
+                    this.id = id;
+                    ctx.Entry(this).State = EntityState.Deleted;
+
+                    ctx.SaveChanges();
+                    rm.SetResponse(true);
+                }
+            }
+
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return rm;
+        }
+
+        public AnexGRIDResponde Listar(AnexGRID grid,  int usuario_id)
+        {
+            try
+            {
+                using (var ctx = new ProyectoContext())
+                {
+
+                    ctx.Configuration.LazyLoadingEnabled = false;
+                    grid.Inicializar();
+
+
+
+                    var query = ctx.Habilidad.Where(x => x.usuario_id == usuario_id); 
+                    
+                    if (grid.columna == "id")
+                    {
+                        query = grid.columna_orden == "DESC" ? query.OrderByDescending(x => x.id) : query.OrderBy(x => x.id);
+                    }
+
+                    if (grid.columna == "nombre")
+                    {
+                        query = grid.columna_orden == "DESC" ? query.OrderByDescending(x => x.nombre) : query.OrderBy(x => x.nombre);
+                    }
+
+                    if (grid.columna == "dominio")
+                    {
+                        query = grid.columna_orden == "DESC" ? query.OrderByDescending(x => x.dominio) : query.OrderBy(x => x.dominio);
+                    }
+
+    
+                    var habilidades = query.Skip(grid.pagina).Take(grid.limite).ToList();
+
+                    var total = query.Count();
+
+                    grid.SetData(habilidades, total);
+
+                }
+            }
+            catch (Exception E)
+            {
+
+                throw;
+            }
+
+            return grid.responde();
+
         }
     }
 }
--- PortafolioMVC/Areas/Admin/Controllers/HabilidadesController.cs
+++ PortafolioMVC/Areas/Admin/Controllers/HabilidadesController.cs
@@ -1,4 +1,6 @@
-﻿using PortafolioMVC.Areas.Admin.Filters;
+﻿using Helper;
+using Model;
+using PortafolioMVC.Areas.Admin.Filters;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,10 +12,51 @@ namespace PortafolioMVC.Areas.Admin.Controllers
    [Autenticado]
     public class HabilidadesController : Controller
     {
+
+        private Habilidad Habilidad = new Habilidad();
         // GET: Admin/Habilidades
         public ActionResult Index()
         {
             return View();
+        }
+
+
+        public JsonResult Listar(AnexGRID grid)
+        {
+            return Json(Habilidad.Listar(grid, SessionHelper.GetUser()));
+        }
+
+        public ActionResult Crud( int id = 0)
+        {
+            if (id == 0) Habilidad.usuario_id = SessionHelper.GetUser(); //GetUser me trae el id del usuario correcto
+            
+            else Habilidad = Habilidad.Obtener(id);
+
+            return View(Habilidad);
+        }
+
+
+        public JsonResult Guardar(Habilidad model)
+        {
+            var rm = new ResponseModel();
+
+            if (ModelState.IsValid)
+            {
+                rm = model.Guardar();
+                if (rm.response)
+                {
+                    rm.href = Url.Content("~/Admin/Habilidades/");
+                }
+            }
+
+            return Json(rm);
+        }
+
+
+        public JsonResult Eliminar(int id)
+        {
+
+            return Json(Habilidad.Eliminar(id));
         }
     }
 }
