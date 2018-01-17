30a64716d6d152497c1021617339fdb9a6b673c1
DiegoJojoa <diegojojoa-94@hotmail.com>
2018-01-17 15:09:54 -0500

    Finalización del FronOffice. Faltan los estilos.

--- PortafolioMVC/Controllers/DefaultController.cs
+++ PortafolioMVC/Controllers/DefaultController.cs
@@ -5,6 +5,9 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using PortafolioMVC.App_Start;
+using PortafolioMVC.ViewsModels;
+using System.Net.Mail;
+using Rotativa.MVC; //Descargar el puglins de nuget RotativaMVC
 
 namespace PortafolioMVC.Controllers
 {
@@ -17,5 +20,69 @@ namespace PortafolioMVC.Controllers
         {
             return View(usuario.Obtener(FronOfficeStartUp.UsuarioVisualizando(), true));
         }
+
+        public JsonResult EnviarCorreo(ContactoViewModel model)
+        {
+            var rm = new ResponseModel();
+
+
+          
+
+           
+            if (ModelState.IsValid)
+            {
+
+                try
+                {
+                    var _usuario = usuario.Obtener(FronOfficeStartUp.UsuarioVisualizando());
+
+                    var mail = new MailMessage();
+                    mail.From = new MailAddress(model.correo,model.nombre);
+                    mail.To.Add(_usuario.email);
+                    mail.Subject = "Correo desde contacto";
+                    mail.IsBodyHtml = true;
+                    mail.Body = model.mensaje;
+
+                    var SmtpServer = new SmtpClient("smpt.live.com"); //or "smtp.gmail.com"
+                    SmtpServer.Port = 587;
+                    SmtpServer.DeliveryMethod = SmtpDeliveryMethod.Network;
+                    SmtpServer.UseDefaultCredentials = false;
+                    SmtpServer.Credentials = new System.Net.NetworkCredential("diegofernando-58@hotmail.com", "Contraseña");
+                    SmtpServer.EnableSsl = true;
+                    SmtpServer.Send(mail);
+
+                }
+                catch (Exception e)
+                {
+
+                    rm.SetResponse(false, e.Message);
+                    return Json(rm);
+
+                    throw;
+                }
+
+
+                rm.SetResponse(true);
+                rm.function = "cerrarContacto();";
+
+
+            }
+
+            return Json(rm);
+        }
+
+
+        public ActionResult ExportarPDF()
+        {
+            return new ActionAsPdf("PDF");
+        }
+
+        public ActionResult PDF()
+        {
+            return View(
+                usuario.Obtener(FronOfficeStartUp.UsuarioVisualizando(), true));
+        }
+
+
     }
 }
---
+++ PortafolioMVC/ViewsModels/ContactoViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PortafolioMVC.ViewsModels
+{
+    public class ContactoViewModel
+    {
+        [Required]
+        public string nombre { get; set; }
+        [Required]
+        [EmailAddress]
+        public string correo { get; set; }
+        [Required]
+        public string mensaje { get; set; }
+    }
+}
